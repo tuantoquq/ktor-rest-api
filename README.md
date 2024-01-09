8100629352799ecfad9df25dffa48b34bc8254bc
Nguyễn Hoàng Anh Tuấn <tuantoquq@gmail.com>
2024-01-09 14:22:51 +0700

    add README file

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ktor Server Restful API
+
+## Features:
+- [x] Authentication
+- [x] Authorization
+- [x] JWT
+- [x] Database
+- [x] HikariCP
+- [x] Redis
+- [x] Dependency Injection
+
+## How to run:
+- [x] Run `docker-compose up -d` to start the database and redis
--- src/test/kotlin/openway/com/ApplicationTest.kt
+++ src/test/kotlin/openway/com/ApplicationTest.kt
@@ -1,39 +1,15 @@
 package openway.com
 
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.serialization.jackson.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.testing.*
-import openway.com.adapter.UserAdapter
-import openway.com.configs.DatabaseFactory
 import kotlin.test.*
-import openway.com.repositories.UserRepository
-import openway.com.repositories.UserRepositoryImpl
-import openway.com.routes.authRoute
-import openway.com.services.UserService
-import openway.com.services.UserServiceImpl
 
 class ApplicationTest {
     @Test
-    fun testRoot() = testApplication {
-        application {
-            install(ContentNegotiation)
-            {
-                jackson()
-            }
-            DatabaseFactory.init()
-            val userRepository: UserRepository = UserRepositoryImpl()
-            val userAdapter: UserAdapter = UserAdapter()
-            val userService: UserService = UserServiceImpl(userRepository, userAdapter)
-            authRoute(userService)
-
-        }
+    fun testHealthCheck() = testApplication {
         client.get("/health-check").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Service is healthy!", bodyAsText())
         }
     }
 }
